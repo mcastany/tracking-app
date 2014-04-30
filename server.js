b9fc35c82156ddac004d790cffad7afee31f5171
Marcos Castany <marcos.castany@southworks.net>
2014-04-30 15:53:27 -0300

    [Configuration] Updated server.js with new version

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
-var app = express.createServer();
+var app = express();
+var env = app.get('env');
+
+var server = app.listen(process.env.PORT || 4730);;
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+if ('production' != env) {
+    app.use(express.errorHandler());
+}
 
 app.get('/', function(req, res) {
   res.type('text/plain');
   res.send('NodeJS Configured in AppHarbor');
 });
 
-app.listen(process.env.PORT || 4730);
+exports = module.exports = app;
+exports.use = function() {
+  app.use.apply(app, arguments);
+};
